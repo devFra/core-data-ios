4c77f5c0ac0c24421ded588fd4d785f0ee774495
Francesco Mariani <francesco.mariani@flowing.it>
2022-10-06 15:12:46 +0200

    Initial Commit

---
+++ study-core-data/study_core_dataApp.swift
@@ -0,0 +1,17 @@
+//
+//  study_core_dataApp.swift
+//  study-core-data
+//
+//  Created by Francesco on 06/10/22.
+//
+
+import SwiftUI
+
+@main
+struct study_core_dataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
